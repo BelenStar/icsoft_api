50f080aaa2039916e5774bef5d0bffd28665cc7d
nelly <estrelladebelen2000@gmail.com>
2019-11-17 20:19:37 -0400

    Se agrego la funcion de selectUserByUsername en el modulo db_logic

--- utils/auth.py
+++ utils/auth.py
@@ -9,9 +9,9 @@ def login():
     results = db_logic.getAllUsers()
     print(results)
     users = []
+    
     for row in results:
         user = us.User(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
         print(user.username)
         users.append(user.serialize())
-    return users
-
+    return users 
