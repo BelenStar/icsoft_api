c75d9cbc70042321fe50093a196de7e3626f1355
nelly <estrelladebelen2000@gmail.com>
2019-11-24 20:51:13 -0400

    Metodo getAllItem, atributo quantity y cambio de nombre al atributo de la foto

--- models/item.py
+++ models/item.py
@@ -2,20 +2,21 @@ from repository import db_logic
 
 class Item():
     #constructor
-    def __init__(self, id, name, category, description, price, pic, provider):
+    def __init__(self, id, name, category, description, price, path_to_picture, provider,quantity):
 
         self.id = id
         self.name = name
         self.category = category
         self.description =  description
         self.price = price
-        self.pic = pic
+        self.path_to_picture = path_to_picture
         self.provider = provider
+        self.quantity = quantity
 
 
     
     def serialize(self):
         return {"id": self.id, "name": self.name, "category": self.category, "description": self.description,
-         "price": self.price, "pic": self.pic, "provider": self.provider}
+         "price": self.price, "path_to_picture": self.path_to_picture, "provider": self.provider, "quantity": self.quantity}
 
         
